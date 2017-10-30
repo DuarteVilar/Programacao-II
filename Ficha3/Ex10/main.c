62ed98f970bd7dcc1a66a4dc29aa7fca1ed818fb
DuarteVilar <duartevilar2@gmail.com>
2017-10-31 00:47:00 +0100

    Ex10

--- Ficha3/Ex10/main.c
+++ Ficha3/Ex10/main.c
@@ -1,22 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: Vilar
- *
- * Created on 30 de Outubro de 2017, 23:41
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
 int main(int argc, char** argv)
 
 {
