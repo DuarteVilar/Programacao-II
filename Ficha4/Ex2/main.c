dd205756304b2e2968778fb7e70d7c4bd9e021fc
DuarteVilar <duartevilar2@gmail.com>
2017-11-07 18:07:29 +0000

    Ex2

--- Ficha4/Ex2/main.c
+++ Ficha4/Ex2/main.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv)
+int n1, n2, operacao;
+double resultado;
 
-{
-    
-    
-
-    return (0);
+long soma(){
+    return n1 + n2;
+}
+double multiplicar(){
+    return n1 * n2;
+}
+long subtrair(){
+    return n1 - n2;
+}
+double dividir(){
+    return (float)n1 / n2;
 }
 
+int main(int argc, char** argv) {
+
+    printf("Escreve o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("Escreve o segundo numero: ");
+    scanf("%d", &n2);
+    
+    printf("1-Somar\n");
+    printf("2-Subtrair\n");
+    printf("3-Mutiplicar\n");
+    printf("4-Dividir\n");
+    printf("Escolhe uma opcao: ");
+    scanf("%d", &operacao);
+     
+    
+    switch (operacao){
+        case 1:
+            resultado=soma();
+            break;
+        case 2:
+            resultado=subtrair();
+            break;
+        case 3:
+            resultado=multiplicar();
+            break;
+        case 4:
+            resultado=dividir();
+            break;
+        default:
+            printf("Operacao Invalida.");      
+    }
+    printf("Resultado: %.2lf", resultado);
+    return (0);
+}
