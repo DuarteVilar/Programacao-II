49ac555c9741c18256d61a8b82737cc8c7a8b8a7
DuarteVilar <duartevilar2@gmail.com>
2017-11-14 18:03:44 +0000

    Ex1 e Ex2

--- Ficha5/Ex2/main.c
+++ Ficha5/Ex2/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAM 20
+#define TAM 26
 
 int main(int argc, char** argv) {
-    int numero[20];
+    int numero[26];
     int x;
     
-    for (x=0; x < TAM; ++x){
+    for (x=5; x < TAM; ++x){
         numero[x]=x;
     }
     
-    for (x=0; x < TAM; ++x){
+    for (x=5; x < TAM; ++x){
         printf("%d ",numero[x]);
     }
     
